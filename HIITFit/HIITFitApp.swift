dce2fc6b94ad154a1dd4a26ec475e1ce6334af8a
cybershen <cyberpuer@proton.me>
2022-12-06 19:37:56 +0200

    Initial Commit

---
+++ HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by Назар Жиленко on 06.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
